712a332f74707f8b0f6c9c84b6eaad4953734d58
Troy Benson <troybensonsa@gmail.com>
2026-01-07 11:49:59 -0700

    zero before returning

--- src/aarch64/glibc_2_35.s
+++ src/aarch64/glibc_2_35.s
@@ -1,5 +1,11 @@
 public function _dl_find_object {
   bti c
+  cbz x1, skip_zero
+  stp xzr, xzr, [x1]
+  stp xzr, xzr, [x1, #16]
+  stp xzr, xzr, [x1, #32]
+  stp xzr, xzr, [x1, #48]
+skip_zero:
   mov x0, #-1
   ret.cfi
 }
--- src/x86_64/glibc_2_35.s
+++ src/x86_64/glibc_2_35.s
@@ -1,5 +1,16 @@
 public function _dl_find_object {
   endbr64
+  test rsi, rsi
+  jz skip_zero
+  mov qword ptr [rsi], 0
+  mov qword ptr [rsi + 8], 0
+  mov qword ptr [rsi + 16], 0
+  mov qword ptr [rsi + 24], 0
+  mov qword ptr [rsi + 32], 0
+  mov qword ptr [rsi + 40], 0
+  mov qword ptr [rsi + 48], 0
+  mov qword ptr [rsi + 56], 0
+skip_zero:
   mov eax, -1
   ret
 }
